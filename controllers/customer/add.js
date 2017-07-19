3341f64da029a74a748cba8ef86c8477e6b01901
jamesmanone <boostedx@me.com>
2017-07-18 20:33:04 -0400

    Refactor customer add

---
+++ controllers/customer/add.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Customer = require('../../models/Customer');
+
+const router = express.Router();
+
+router.put('/new', (req, res) => {
+  if(!checkPermissions(req.user, 2)) return lowPermissions(res);
+  const { name } = req.body;
+  if(!name) {
+    return res.json({
+      success: false,
+      msg: 'Name is required'
+    });
+  }
+  const customer = new Customer({name: name})
+  customer.save();
+  return res.json({
+    success: true,
+    customer: customer
+  });
+});
+
+module.exports = router;
--- controllers/customer/index.js
+++ controllers/customer/index.js
@@ -12,23 +12,7 @@ router.get('/', (req, res) => {
     .then(customers => res.json(customers));
 });
 
-router.put('/new', (req, res) => {
-  if(!checkPermissions(req.user, 2)) return lowPermissions(res);
-  const { name } = req.body;
-  if(!name) {
-    return res.json({
-      success: false,
-      msg: 'Name is required'
-    });
-  }
-  const customer = new Customer({name: name})
-  customer.save();
-  return res.json({
-    success: true,
-    customer: customer
-  });
-});
-
+router.use(require('./add'));
 router.use(require('./phone'));
 router.use(require('./address'));
 router.use(require('./comment'));
